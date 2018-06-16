b2f11c1639229d7ff98a3a96d70706c975a74701
Owner <owner@Owners-MBP.fios-router.home>
2018-06-16 02:42:55 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,5 +1,18 @@
 console.log("Up and running!");
-var cards = ["queen", "queen", "king", "king"];
+var cards = [ 
+{rank: "queen",
+ suit: "hearts",
+ cardImage: "images/queen-of-hearts.png"},
+{rank: "queen",
+ suit: "diamonds",
+ cardImage: "images/queen-of-diamonds.png"}, 
+{rank: "king",
+ suit: "hearts",
+ cardImage: "images/king-of-hearts.png"}, 
+{rank: "king",
+ suit: "diamonds",
+ cardImage: "images/king-of-diamonds.png"}
+];
 var cardsInPlay = []; 
 var checkForMatch = function () {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
@@ -8,8 +21,10 @@ var checkForMatch = function () {
 		console.log("Sorry, try again.");}
 	}
 var flipCard = function (cardId) {
-	cardsInPlay.push(cards[cardId]);
-	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId].rank);
+	console.log("User flipped " + cards[cardId].rank);
+	console.log("User flipped " + cards[cardId].suit);
+	console.log("User flipped " + cards[cardId].cardImage);
 	{if (cardsInPlay.length === 2) {
 		checkForMatch ();} } 
 }
